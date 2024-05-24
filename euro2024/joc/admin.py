b076335c826f50ce5fdbfd3f6b50523d6e6e9c86
Xavier Janer <xjaner@gmail.com>
2024-05-25 01:00:48 +0200

    Fet l'admin més pràctic

--- euro2024/joc/admin.py
+++ euro2024/joc/admin.py
@@ -11,8 +11,20 @@ def approve_players(modeladmin, request, queryset):
 
 class JugadorAdmin(admin.ModelAdmin):
     actions = [approve_players]
-    readonly_fields = ('usuari', 'posicio', 'posicio_anterior', 'punts', 'punts_anterior',
-                       'punts_resultats', 'punts_grups', 'punts_equips_encertats')
+    list_display = ['usuari', 'get_first_name', 'get_is_active', 'get_date_joined']
     list_filter = ['usuari__is_active']
+    ordering = ['-usuari__date_joined']
+
+    @admin.display(description='Nom Real')
+    def get_first_name(self, obj):
+        return obj.usuari.first_name
+
+    @admin.display(description='Registrat')
+    def get_date_joined(self, obj):
+        return obj.usuari.date_joined
+
+    @admin.display(description='Activat?')
+    def get_is_active(self, obj):
+        return obj.usuari.is_active
 
 admin.site.register(Jugador, JugadorAdmin)
