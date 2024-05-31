fcac684d342edea367768e297998e43563d68e29
Xavier Janer <xjaner@gmail.com>
2024-06-01 00:04:03 +0200

    Esborrada la final de consolació de la puntuació, i agafat l'ide l'admin dels settings

--- euro2024/joc/views/classificacio.py
+++ euro2024/joc/views/classificacio.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from django.conf import settings
 from django.db.models import Q, F
 from django.views import generic
 
@@ -9,7 +10,7 @@ class ClassificacioView(generic.ListView):
     template_name = "joc/classificacio.html"
 
     def get_queryset(self):
-        return Jugador.objects.filter(usuari__is_active=True).filter(~Q(usuari_id=1)).annotate(
+        return Jugador.objects.filter(usuari__is_active=True).filter(~Q(usuari_id=settings.ID_ADMIN)).annotate(
             dif_pos=F('posicio_anterior') - F('posicio')).annotate(
             dif_punts=F('punts') - F('punts_anterior')).order_by('posicio')
 
--- euro2024/joc/views/usuaris.py
+++ euro2024/joc/views/usuaris.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.conf import settings
 from django.views import generic
 
 from joc.models import Jugador
@@ -6,4 +7,4 @@ from joc.models import Jugador
 
 class UsuarisView(generic.ListView):
     def get_queryset(self):
-        return Jugador.objects.filter(usuari__is_active=True).filter(~Q(usuari_id=1))
+        return Jugador.objects.filter(usuari__is_active=True).filter(~Q(usuari_id=settings.ID_ADMIN))
